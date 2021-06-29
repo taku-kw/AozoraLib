260fa5fb081a5e0ea8df5d077ca6b4004eff1459
kawamoto <riverbooktable@gmail.com>
2021-06-29 21:27:33 +0900

    Add loading anime

--- app/javascript/packs/lib/lib.js
+++ app/javascript/packs/lib/lib.js
@@ -20,3 +20,13 @@ $('.item-area-1 a')
     $('.item-area-3').css('display', 'none')
     $('.item-area-2').css('display', 'none')
   })
+
+$('.search-item')
+  .on('click', function(){
+    $('#result-area').append('<div id=#loading-search></div>')
+  })
+
+  $('.result-area')
+    .on('click', '.search-item-author', function() {
+      $('#result-area').append('<div id=#loading-search></div>')
+  })
